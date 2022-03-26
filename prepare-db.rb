f1e059f74a2484a90fd57a1be4b8109693a9132f
Karol Preiskorn <karol.preiskorn@users.noreply.github.com>
2022-03-26 17:55:07 +0000

    update configurtion for postgres, VSC, ruby

--- prepare-db.rb
+++ prepare-db.rb
@@ -1,31 +1,58 @@
+
+require "sequel"
+
+# connect to an in-memory database
+DB = Sequel.postgres
+
 # you can use Sequel (https://github.com/jeremyevans/sequel) to setup your database.
 # The connection is already made for you.
 
 # You can also move this code into the preloaded section so that its available
 # for all tests (including examples).
 
-DB.create_table: students do
-primary_key: Id
-String: FirstName
-String: LastName
-boolean: IsActive
+DB.create_table :students do
+  primary_key :Id
+  String :FirstName
+  String :LastName
+  boolean :IsActive
 end
 
 students = DB[:students]  # Create a dataset
 
-# Populate the table
-# You can use Faker (https://github.com/stympy/faker) to generate data
-4.times do
-  students.insert(FirstName: Faker:: Name.first_name, LastName: Faker: : Name.last_name, IsActive: true)
-end
-6.times do
-  students.insert(FirstName: Faker:: Name.first_name, LastName: Faker: : Name.last_name, IsActive: false)
+# create an items table
+DB.create_table :items do
+  primary_key :id
+  String :name, unique: true, null: false
+  Float :price, null: false
 end
 
-results = run_sql
+# create a dataset from the items table
+items = DB[:items]
 
-describe: students do
-it "should return 4 students" do
-expect(results.count).to eq 4
-end
-end
+# populate the table
+items.insert(name: 'abc', price: rand * 100)
+items.insert(name: 'def', price: rand * 100)
+items.insert(name: 'ghi', price: rand * 100)
+
+# print out the number of records
+puts "Item count: #{items.count}"
+
+# print out the average price
+puts "The average price is: #{items.avg(:price)}"
+
+# # Populate the table
+# # You can use Faker (https://github.com/stympy/faker) to generate data
+# 4.times do
+#   students.insert(FirstName: Faker:: Name.first_name, LastName: Faker: : Name.last_name, IsActive : true)
+# end
+# 6.times do
+#   students.insert(FirstName: Faker:: Name.first_name, LastName: Faker: : Name.last_name, IsActive : false)
+# end
+
+# results = run_sql
+
+# describe: students do
+# it "should return 4 students" do
+# expect(results.count).to eq 4
+# end
+# end
